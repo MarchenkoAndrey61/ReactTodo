7933c25d9c2416f38bc046c4a89d3147fe8e7d4e
MarchenkoAndrey61 <xayc755@yandex.ru>
2019-12-06 14:43:51 +0300

    add style for task

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,22 @@
 import React from "react"
 
 function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input type="checkbox" 
-                   checked={props.item.completed}
-                   onChange={() => props.handleChange(props.item.id)}/>
-            <p>{props.item.text}</p>
-        </div>
-    )
+  const completedStyle = {
+      fontStyle: "italic",
+      color: "#cdcdcd",
+      textDecoration: "line-through"
+  }
+  
+  return (
+      <div className="todo-item">
+          <input 
+              type="checkbox" 
+              checked={props.item.completed} 
+              onChange={() => props.handleChange(props.item.id)}
+          />
+          <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
+      </div>
+  )
 }
 
 export default TodoItem
